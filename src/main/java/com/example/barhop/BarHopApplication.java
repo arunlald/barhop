45f5eeb3255ec4f37d20e12dda49d826fceb4a0f
arunlald <arunlald18@gmail.com>
2022-07-16 11:47:16 -0700

    .

---
+++ src/main/java/com/example/barhop/BarHopApplication.java
@@ -0,0 +1,13 @@
+package com.example.barhop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarHopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarHopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/barhop/BarHopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.barhop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarHopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
